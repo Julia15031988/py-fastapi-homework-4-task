331eceada1937d575dc24abcec831eb6455c98ae
Okhrymenko <Ohrimenko15@gmail.com>
2025-10-19 13:02:57 +0300

    solution

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -6,7 +6,7 @@ from validation import (
     validate_birth_date,
     validate_image,
 )
-from fastapi import Form, UploadFile, File, HTTPException
+from fastapi import Form, UploadFile, File, HTTPException, APIRouter, Form, Depends, HTTPException, Request, status
 
 
 class ProfileCreateRequestSchema(BaseModel):
