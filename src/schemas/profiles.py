9ab25773d0255ec74ccb9c7bb73065047f2c3bc2
Okhrymenko <Ohrimenko15@gmail.com>
2025-10-19 10:49:01 +0300

    solution  git commit -amsolution

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -59,7 +59,7 @@ class ProfileCreateRequestSchema(BaseModel):
     @field_validator("info")
     @classmethod
     def validate_info(cls, value: str) -> str:
-        if not value.strip():
+        if not value or not value.strip():
             raise ValueError("Info must not be empty.")
         return value
 
