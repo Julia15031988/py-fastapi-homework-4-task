a4218d58b25fa30913eb4ca306be1e5d2b3effc2
Okhrymenko <Ohrimenko15@gmail.com>
2025-10-20 21:11:04 +0300

    solution

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter
 from fastapi import Form, File, UploadFile, HTTPException, status
 from pydantic import BaseModel, field_validator, ConfigDict
 from datetime import date
-from validation import validate_name, validate_gender, validate_birth_date
+from validation import (
+    validate_name,
+    validate_image,
+    validate_gender,
+    validate_birth_date
+)
 
 router = APIRouter()
-
 
 
 class ProfileCreateRequestSchema(BaseModel):
